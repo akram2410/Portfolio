a72703e2682eb92ca43b210006d9def90710adbc
Akram Mohammad <akram_mohammad1@persistent.co.in>
2020-03-31 17:15:06 +0530

    Updates

---
+++ precache-manifest.0079b72d8fbdde4f18f5d1997258b136.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fca8201b394590056d5cc63b26ec48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db418fa48f575a97ebca",
+    "url": "./static/css/main.f2464cc6.chunk.css"
+  },
+  {
+    "revision": "0e9ead5766bdb7b2a2e6",
+    "url": "./static/js/2.15380064.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "./static/js/2.15380064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db418fa48f575a97ebca",
+    "url": "./static/js/main.ceea89ca.chunk.js"
+  },
+  {
+    "revision": "d8c7886c3d147ceaa4f1",
+    "url": "./static/js/runtime-main.f7d5131d.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "./static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "./static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
